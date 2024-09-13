218a8fb7529a8a28c13937e0ec41f1fef4452d01
gen <kyliki@yandex.ru>
2024-09-13 23:07:08 +0300

    burger animation

--- include/header/header.php
+++ include/header/header.php
@@ -3,9 +3,9 @@
     <div class="header__title">Ледограф</div>
     <div class="burger">
       <button class="burger__btn btn-reset" id="btn-nav-open" aria-label="открыть навигацию">
-      <span class="burger__line"></span>
-      <span class="burger__line"></span>
-      <span class="burger__line"></span>
+        <span class="burger__line"></span>
+        <span class="burger__line"></span>
+        <span class="burger__line"></span>
       </button>
     </div>
   </div>
--- js/navigation.js
+++ js/navigation.js
@@ -3,20 +3,31 @@
   const navPanel = document.getElementById("nav");
   const body = document.getElementById("body");
   const overlay = document.getElementById("overlay");
+  const burger = document.querySelector(".burger");
 
   btnNavOpen.addEventListener("click", () => {
-    navPanel.classList.toggle("nav--close");
-    body.classList.toggle("modal-opened");
-    overlay.classList.toggle("modal-opened");
+    transformBurger();
+    toggleNavPanel();
     overlay.addEventListener("click", closeNavPanel);
   });
 
-  btnNavClose.addEventListener("click", closeNavPanel);
+  function toggleNavPanel() {
+    navPanel.classList.toggle("nav--close");
+    body.classList.toggle("modal-opened");
+    overlay.classList.toggle("modal-opened");
+  }
 
   function closeNavPanel() {
     navPanel.classList.remove("nav__panel--open");
     body.classList.remove("modal-opened");
     overlay.classList.remove("modal-opened");
     overlay.removeEventListener("click", closeNavPanel);
+  }
+
+  function transformBurger() {
+    if (!burger.classList.contains("transform-to-burger")) {
+      burger.classList.add("transform-to-burger");
+    }
+    burger.classList.toggle("transform-to-cross");
   }
 })();
